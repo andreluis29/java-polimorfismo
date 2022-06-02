a7f3af8ea3408a727fd3814ce24c73be1f719a6e
andreluis29 <andre.wenningkamp1@gmail.com>
2022-06-02 10:13:40 -0300

    Parte 6 concluída

--- java-polimorfismo/src/EditorVideo.java
+++ java-polimorfismo/src/EditorVideo.java
@@ -1,7 +1,19 @@
-public class EditorVideo extends Funcionario {
+public class EditorVideo extends Funcionario implements Autenticavel {
     @Override
     public double pegarBonificacao() {
         return pegarBonificacao() + super.getSalario();
+    }
+
+    @Override
+    public void setSenha(int senha) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean autentica(int senha) {
+        // TODO Auto-generated method stub
+        return false;
     }
 
 }
--- java-polimorfismo/src/Gerente.java
+++ java-polimorfismo/src/Gerente.java
@@ -1,25 +1,27 @@
-public class Gerente extends Funcionario {
+public class Gerente extends Funcionario implements Autenticavel {
     private int senha;
-
-    public boolean autentica(int senha) {
-        if (this.senha == senha) {
-            return true;
-        }
-        return false;
-    }
 
     @Override
     public double pegarBonificacao() {
         return pegarBonificacao() + super.getSalario();
     }
 
-
     public int getSenha() {
         return this.senha;
     }
 
+    @Override
     public void setSenha(int senha) {
         this.senha = senha;
+        
+    }
+
+    @Override
+    public boolean autentica(int senha) {
+        if (this.senha == senha) {
+            return true;
+        }
+        return false;
     }
 
 }
